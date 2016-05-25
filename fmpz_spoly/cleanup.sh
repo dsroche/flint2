435aa8797dd68b73e0937e2f4dd4c33c37a75396
Daniel S. Roche <roche@usna.edu>
2016-05-25 14:03:33 -0700

    Revert "Revert "cleanup script""
    This reverts commit 8baaf5e73c7b5daac9976ea0f9ea83534cf9688e.

---
+++ fmpz_spoly/cleanup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash --norc
+
+set -e
+set -u
+
+srcdir=$(dirname "$(readlink -f "$0")")
+
+function usage {
+  echo "Usage: $0"
+  echo "Removes all the extra files and gets ready for \"public view\" of fmpz_spoly"
+  echo "WARNING: This script will self-destruct!"
+  [[ $# -eq 1 ]] && exit $1
+}
+
+[[ $# -eq 0 ]] || usage 1
+
+cd "$srcdir"
+
+exec 4<<DELETEFILES
+TODO.txt
+ourtest
+Makefile
+# cleanup.sh
+DELETEFILES
+
+while read -u4 filename; do
+    if [[ $filename =~ ^# ]]; then
+        echo "comment: $filename"
+    elif [[ $filename =~ ^[/.] ]]; then
+        echo "ILLEGAL FILENAME: $filename (skipping)"
+    elif [[ -d $filename ]]; then
+        rm -r "$filename/"
+    elif [[ -e $filename ]]; then
+        rm "$filename"
+    fi
+done
+
+exec 4<&-
+
+sed -i 's/^\#\( *fmpz_spoly\)/ \1/' ../Makefile.in
